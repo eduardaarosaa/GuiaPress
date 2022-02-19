c1c51784732667d2880f9790bdd57666920c6e9f
Eduarda <eduarda@houpa.app>
2022-02-19 17:31:40 -0300

    Add route de delete category

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -29,4 +29,35 @@ router.post("/categories/save", (req,res) => {
     }
 });
 
+router.get("/admin/categories", (req,res) => {
+
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {
+            categories: categories
+        });
+    });
+});
+
+router.post("/categories/delete", (req,res) => {
+    var id = req.body.id;
+
+    if(id != undefined){
+        
+        if(!isNaN(id)){
+            Category.destroy({
+                where: {
+                    id:id
+                  }   
+              }).then(() => {
+                  res.redirect("/admin/categories")
+              });
+
+        }else{//NÃO for um número
+            res.redirect("/admin/categories");
+        }
+    }else{//NULL
+        res.redirect("/admin/categories");
+    }
+});
+
 module.exports = router ;
