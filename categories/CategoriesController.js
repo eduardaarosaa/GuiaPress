c9e9f126e01e02ca788892058263696412ae4418
Eduarda <eduarda@houpa.app>
2022-02-19 18:39:51 -0300

    CRUD categorias Ok

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -22,7 +22,7 @@ router.post("/categories/save", (req,res) => {
             title:title,
             slug: slugify(title)
         }).then(()=> {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     }else{
         res.redirect("/admin/categories/new");
@@ -42,7 +42,7 @@ router.post("/categories/delete", (req,res) => {
     var id = req.body.id;
 
     if(id != undefined){
-        
+
         if(!isNaN(id)){
             Category.destroy({
                 where: {
@@ -58,6 +58,40 @@ router.post("/categories/delete", (req,res) => {
     }else{//NULL
         res.redirect("/admin/categories");
     }
+});
+
+router.get("/admin/categories/edit/:id", (req,res) => {
+
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    //Pesquisar algo pelo ID - rapido
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+
+            res.render("admin/categories/edit", {category:category});
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+});
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({title:title, slug: slugify(title)}, {
+        where:{
+            id:id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
 });
 
 module.exports = router ;
